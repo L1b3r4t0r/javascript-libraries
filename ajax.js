fa6ca12553ea79724a00d07771d245dfb981abca
matheus <sandboxm@outlook.com>
2013-11-07 23:41:05 -0200

    first upload

---
+++ ajax.js
@@ -0,0 +1,23 @@
+//(c) matheus 2013
+(function ajf() {
+    var xmlhttp;
+    this.ajax = ajax;
+    function ajax(url, callback, data, method) {
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        };
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                eval(callback);
+            };
+        };
+        var res = method.toUpperCase();
+        xmlhttp.open(res, url + "?" + data, true);
+        xmlhttp.send();
+    };
+});
+function makecall(url,callback){
+    var instance = new ajf.ajax();
+};
